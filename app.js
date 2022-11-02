b6b520857118e7aa7914168e85f45f44b87b5e1a
Nikithavedanth <nikithavedanth@gmail.com>
2022-11-01 22:08:53 -0500

    updated

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var AtlasRouter = require('./routes/Atlas');
+var selectorRouter = require('./routes/selector');
+var gridbuildRouter = require('./routes/gridbuild');
 
 var app = express();
 
@@ -23,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/Atlas', AtlasRouter);
+app.use('/selector', selectorRouter);
+app.use('/gridbuild', gridbuildRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
